f3aaf0c6bf53b4de14380033798d840b417771d6
fzyier <babeckiy2004@gmail.com>
2022-10-05 13:38:15 +0300

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
+	int a, b;
 
+	a = 20;
+	b = a;
+	while (b != 1000) {
+		b++;
+		cout << a << "\n";
+		a += b;
+	}
+	cout << "End: " << a << endl;
+	cout << "a / 1000 = " << a / 1000 << endl;
 	return 0;
 }
