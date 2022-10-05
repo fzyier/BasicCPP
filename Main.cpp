f83f38230f464298f78333a008d21899949abee7
fzyier <babeckiy2004@gmail.com>
2022-10-05 13:32:41 +0300

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 
 int main() {
+	int a, b, d;
+	cout << "Enter A: ";
+	cin >> a;
+	cout << "Enter B: ";
+	cin >> b;
 
+	do
+	{
+		cout << "Enter D: ";
+		cin >> d;
+
+		if (a > b) {
+			(d < a&& d > b) ? cout << "Alright!\n" : cout << "Nope!\n";
+			break;
+		}
+		else if (a < b) {
+			(d > a && d < b) ? cout << "Alright!\n" : cout << "Nope!\n";
+			break;
+		}
+
+	} while (true);
 	return 0;
 }
