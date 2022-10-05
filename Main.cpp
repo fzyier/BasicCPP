559ac5d70b29845123d65687eefe65ca1add9218
fzyier <babeckiy2004@gmail.com>
2022-10-05 12:43:53 +0300

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main() {
-	cout << "Hello world!" << endl;
+	int x, y;
+	cout << "Enter x: ";
+	cin >> x;
+	y = 0;
+	while (y != x) {
+		cout << ++y << endl;
+	}
 	return 0;
 }
