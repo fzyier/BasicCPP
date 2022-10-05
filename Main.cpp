ce8ed10202ea23c8f014ebf3d8247ae783365c68
fzyier <babeckiy2004@gmail.com>
2022-10-05 13:03:23 +0300

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
-
+	int x, y, z;
+	cout << "Enter x and y: " << endl;
+	cin >> x >> y;
+	z = x;
+	while (z != y) {
+		z++;
+		x = x + z;
+		cout << x << " ";
+	}
 	return 0;
 }
