f39ed2d236a0f5ef097ebb62a8348b81ecb6fe3e
fzyier <babeckiy2004@gmail.com>
2022-10-05 13:06:39 +0300

    Exercise 4

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
-
+	int x, y, z;
+	cout << "Enter x and y: " << endl;
+	cin >> x >> y;
+	cout << "Enter 0 if you want to know: ";
+	cin >> z;
+	if (z == 0)
+		cout << x + y;
+	else
+		cout << "Ok, you don't want to know";
 	return 0;
 }
