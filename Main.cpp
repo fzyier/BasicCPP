e25c24994865ce8f592e0cd3c4d987e6cc6b0311
fzyier <babeckiy2004@gmail.com>
2022-10-05 13:22:59 +0300

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
+	int a, b;
+	cout << "Enter a: ";
+	cin >> a;
 
+	b = a;
+	while (b != 500) {
+		b++;
+		cout << a << "\n";
+		a += b;
+	}
+	cout << "End: " << a << endl;
 	return 0;
 }
