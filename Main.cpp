9bbeb5af4c66d972e17e4d64775007be30f41a63
fzyier <babeckiy2004@gmail.com>
2022-10-10 16:09:52 +0300

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main() {
-
+	int size;
+	string f = "";
+	string s = "";
+	cout << "Enter size:";
+	cin >> size;
+	for (int i = 0; i < size; i++)
+		f += "*";
+	for (int i = 0; i < size; i++)
+		s += "_";
+	cout << f + s + f + s + f + s + f + s << endl;
+	cout << f + s + f + s + f + s + f + s << endl;
+	cout << f + s + f + s + f + s + f + s << endl;
+	cout << s + f + s + f + s + f + s + f << endl;
+	cout << s + f + s + f + s + f + s + f << endl;
+	cout << s + f + s + f + s + f + s + f << endl;
 	return 0;
 }
