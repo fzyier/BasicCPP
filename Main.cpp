7faac20a0db3180ecc83b05916d3164b80d83b63
fzyier <babeckiy2004@gmail.com>
2022-10-10 13:22:52 +0300

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -3,26 +3,44 @@
 using namespace std;
 
 int main() {
-	int a, b, d;
-	cout << "Enter A: ";
+	cout << "What do you want to see?" << endl;
+	cout << "0) *** 1) *   2) *** 3)      4) ***" << endl;
+	cout << "    **    **      *      *       * " << endl;
+	cout << "     *    ***           ***     ***" << endl;
+	cout << "                                   " << endl;
+	cout << "5) * * 6) *   7)   * 8) ***  9)   *" << endl;
+	cout << "   ***    **      **    **       **" << endl;
+	cout << "   * *    *        *    *       ***" << endl;
+
+	int a;
+	cout << endl << "Enter: ";
 	cin >> a;
-	cout << "Enter B: ";
-	cin >> b;
-
-	do
-	{
-		cout << "Enter D: ";
-		cin >> d;
-
-		if (a > b) {
-			(d < a&& d > b) ? cout << "Alright!\n" : cout << "Nope!\n";
-			break;
+	for (int i = 0; i <= 2; ++i) {
+		for (int j = 0; j <= 2; ++j) {
+			switch (a) {
+			case 0:(i - j <= 0) ? cout << "*" : cout << " ";
+				break;
+			case 1:(i - j >= 0) ? cout << "*" : cout << " ";
+				break;
+			case 2:(i < 1 || i <= 1 && i == j ) ? cout << "*" : cout << " ";
+				break;
+			case 3:(i > 1 || i >= 1 && i == j) ? cout << "*" : cout << " ";
+				break;
+			case 4:(i < 1 || i == j || i > 1) ? cout << "*" : cout << " ";
+				break;
+			case 5:(j == 0 || j == 2 || j == i) ? cout << "*" : cout << " ";
+				break;
+			case 6:(j == 0 || i == 1 && j != 2) ? cout << "*" : cout << " ";
+				break;
+			case 7:(j == 2 || i == 1 && j != 0) ? cout << "*" : cout << " ";
+				break;
+			case 8:(i + j <= 2) ? cout << "*" : cout << " ";
+				break;
+			case 9:(i + j >= 2) ? cout << "*" : cout << " ";
+				break;
+			}
 		}
-		else if (a < b) {
-			(d > a && d < b) ? cout << "Alright!\n" : cout << "Nope!\n";
-			break;
-		}
-
-	} while (true);
+		cout << endl;
+	}
 	return 0;
 }
