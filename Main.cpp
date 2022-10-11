ea4b6ae032c097b70f3503394fec281ee06c7a87
fzyier <babeckiy2004@gmail.com>
2022-10-11 06:31:49 +0300

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
 int main() {
-
+	const int length = 10;
+	int array[length];
+	int max = 0, min = length;
+	srand(time(NULL));
+	for (int i = 0; i < length; i++)
+		array[i] = rand() % 11;
+	for (int i = 0; i < length; i++) {
+		if (array[i] > max) {
+			max = array[i];
+		}
+		else if (array[i] < min) {
+			min = array[i];
+		}
+	}
+	cout << max << endl << min << endl;
 	return 0;
 }
