35fc3791202be8879fdda44806cd483166cd9fcb
fzyier <babeckiy2004@gmail.com>
2022-10-11 18:32:23 +0300

    Exercise 5

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <ctime>
+#include <stdlib.h>
 
 using namespace std;
 
 int main() {
+	srand(time(NULL));
+	const int size = 20;
+	//cout << "Size for an array: ";
+	//cin >> size;
+	int array[size];
+	for (int i = 0; i < size; i++)
+		array[i] = rand() % 21 + -10 ;
+
+	for (int i = 0; i < size; i++)
+		cout << array[i] << " ";
 
 	return 0;
 }
