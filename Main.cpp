bbf6b62d8d672e6c272238792c6e5d3a87558587
fzyier <babeckiy2004@gmail.com>
2022-10-11 19:39:20 +0300

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <stdlib.h>
+#include <ctime>
 
 using namespace std;
 
 int main() {
+	const int col = 6;
+	const int row = 2;
+	int array[row][col];
+
+	srand(time(0));
+
+	for (int i = 0; i < row; i++)
+		for (int j = 0; j < col; j++)
+			array[i][j] = rand() % 10;
+
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			cout << array[i][j] << endl;
+		}
+		cout << "\t";
+	}
 
 	return 0;
 }
