955f9d5aaa43c1b4be3bd334cc34c9502d905f4c
fzyier <babeckiy2004@gmail.com>
2022-10-11 19:19:12 +0300

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 int main() {
+	const int col = 5;
+	const int row = 5;
+	int array[col][row];
 
+	for (int i = 0; i < 5; i++) {
+		int first;
+		cout << "Enter: ";
+		cin >> first;
+
+		array[i][0] = first;
+		for (int j = 1; j < 5; j++) {
+			array[i][j] = array[i][j - 1] * 2;
+		}
+	}
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++)
+			cout << array[i][j] << endl;
+	}
 	return 0;
 }
