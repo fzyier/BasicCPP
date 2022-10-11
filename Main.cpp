0c55a669e92c24614575ffe1a970a6d16751513d
fzyier <babeckiy2004@gmail.com>
2022-10-11 19:42:08 +0300

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -7,19 +7,19 @@ int main() {
 	const int row = 5;
 	int array[col][row];
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < col; i++) {
 		int first;
 		cout << "Enter: ";
 		cin >> first;
 
 		array[i][0] = first;
-		for (int j = 1; j < 5; j++) {
+		for (int j = 1; j < row; j++) {
 			array[i][j] = array[i][j - 1] * 2;
 		}
 	}
 
-	for (int i = 0; i < 5; i++) {
-		for (int j = 0; j < 5; j++)
+	for (int i = 0; i < col; i++) {
+		for (int j = 0; j < row; j++)
 			cout << array[i][j] << " ";
 		cout << "\t" << endl;
 	}
