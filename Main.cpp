9fb16eac000c0715e4245acb736bc156e90778c5
fzyier <babeckiy2004@gmail.com>
2022-10-11 20:42:48 +0300

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -7,22 +7,22 @@ using namespace std;
 int main() {
 	const int col = 6;
 	const int row = 2;
-	int array[row][col];
+	int arr[row][col];
 
-	srand(time(0));
+	srand(time(NULL));
 
 	for (int i = 0; i < row; i++)
 		for (int j = 0; j < col; j++)
-			array[i][j] = rand() % 10;
+			arr[i][j] = rand() % 10;
 
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < col; j++)
-			cout << array[i][j] << " ";
+			cout << arr[i][j] << " ";
 		cout << "\t" << endl;
 	}
 
 	int move;
-	cout << "1 - down\n2 - up\n3 - left\n4 - right\n> ";
+	cout << "1 - right\n2 - left\n3 - up\n4 - down\n> ";
 	cin >> move;
 
 	int value;
@@ -31,13 +31,66 @@ int main() {
 
 	switch (move) {
 	case 1:
-
+		for (int i = 0; i < value; i++)
+			cout << arr[0][col - (value - i)] << " ";
+		for (int i = 0; i < col - value; i++)
+			cout << arr[0][i] << " ";
+		cout << endl;
+		for (int i = 0; i < value; i++)
+			cout << arr[1][col - (value - i)] << " ";
+		for (int i = 0; i < col - value; i++)
+			cout << arr[1][i] << " ";
 		break;
 	case 2:
+		for (int i = value; i < col; i++)
+			cout << arr[0][i] << " ";
+		for (int i = 0; i < value; i++)
+			cout << arr[0][i] << " ";
+		cout << endl;
+		for (int i = value; i < col; i++)
+			cout << arr[1][i] << " ";
+		for (int i = 0; i < value; i++)
+			cout << arr[1][i] << " ";
 		break;
 	case 3:
+		if (value % 2 == 0) {
+			for (int i = 0; i < col; i++) {
+				cout << arr[0][i] << " ";
+			}
+			cout << "\t" << endl;
+			for (int i = 0; i < col; i++) {
+				cout << arr[1][i] << " ";
+			}
+		}
+		else {
+			for (int i = 0; i < col; i++) {
+				cout << arr[1][i] << " ";
+			}
+			cout << "\t" << endl;
+			for (int i = 0; i < col; i++) {
+				cout << arr[0][i] << " ";
+			}
+		}
 		break;
 	case 4:
+		if (value % 2 == 0) {
+			for (int i = 0; i < col; i++) {
+				cout << arr[0][i] << " ";
+			}
+			cout << "\t" << endl;
+			for (int i = 0; i < col; i++) {
+				cout << arr[1][i] << " ";
+			}
+		}
+		else {
+			for (int i = 0; i < col; i++) {
+				cout << arr[1][i] << " ";
+			}
+			cout << "\t" << endl;
+			for (int i = 0; i < col; i++) {
+				cout << arr[0][i] << " ";
+			}
+		}
 		break;
 	default:
 		break;
