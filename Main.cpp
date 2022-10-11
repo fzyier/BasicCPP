85c85436402a0accafbb423fbee606461c110183
fzyier <babeckiy2004@gmail.com>
2022-10-11 19:49:08 +0300

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -18,7 +18,29 @@ int main() {
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < col; j++)
 			cout << array[i][j] << " ";
-		cout << "\n\t" << endl;
+		cout << "\t" << endl;
+	}
+
+	int move;
+	cout << "1 - down\n2 - up\n3 - left\n4 - right\n> ";
+	cin >> move;
+
+	int value;
+	cout << "How many?\n> ";
+	cin >> value;
+
+	switch (move) {
+	case 1:
+
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	default:
+		break;
 	}
 
 	return 0;
