3061fafc8263bb02076e295a2870de139c0b48b7
fzyier <babeckiy2004@gmail.com>
2022-10-12 15:21:30 +0300

    Exercise 4

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <stdlib.h>
+#include <ctime>
 
 using namespace std;
 
 int main() {
+	const int size = 5;
+	int arr[size] = {1,2,3,4,5};
+
+	for (int i = 1; i <= size; i++)
+		cout << arr[size-i] << " ";
 
 	return 0;
 }
