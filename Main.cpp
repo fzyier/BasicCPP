a9286b610a564155164f81ebf774536ba31b1ab0
fzyier <babeckiy2004@gmail.com>
2022-10-12 15:33:56 +0300

    Exercise 5

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
+	const int size = 5;
+	int pentagon[size];
 
+	for (int i = 0; i < size; i++) {
+		cout << i + 1 << " side - ";
+		cin >> pentagon[i];
+	}
+
+	int perimeter = 0;
+
+	for (int i = 0; i < size; i++)
+		perimeter += pentagon[i];
+	cout << "Perimeter is " << perimeter;
 	return 0;
 }
