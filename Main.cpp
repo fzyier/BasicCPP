d8369b9eb7226928a70c9f2dd81aca59cbd52d7b
fzyier <babeckiy2004@gmail.com>
2022-10-12 15:08:31 +0300

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,20 @@
 using namespace std;
 
 int main() {
+	const int size = 6;
+	int profit[size];
+
+	int income;
+	for (int i = 0; i < size; i++) {
+		cout << "Enter your income the " << i+1 << " month\n>";
+		cin >> profit[i];
+	}
+
+	int totalIncome = 0;
+	for (int j = 0; j < size; j++)
+		totalIncome += profit[j];
+	
+	cout << "For 6 months: " << totalIncome;
 
 	return 0;
 }
