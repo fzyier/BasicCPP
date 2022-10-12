17dc3219669d7832cb2fbbc2f3d9e9da423ffcff
fzyier <babeckiy2004@gmail.com>
2022-10-12 17:51:25 +0300

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <ctime>
+#include <stdlib.h>
+
 
 using namespace std;
 
 int main() {
+	srand(time(NULL));
+	const int col = 2, row = 5;
+	int arr[col][row];
+	for (int i = 0; i < col; i++) {
+		for (int j = 0; j < row; j++) {
+			arr[i][j] = 1 + rand() % 10;
+		}
+	}
 
+	int sum = 0, min = arr[0][0], max = arr[0][0];
+	for (int i = 0; i < col; i++) {
+		for (int j = 0; j < row; j++) {
+			sum += arr[i][j];
+			if (min > arr[i][j])
+				min = arr[i][j];
+			if (max < arr[i][j])
+				max = arr[i][j];
+		}
+	}
+	cout << "Sum: " << sum << endl << "Average: " << (float)sum / (row*col) << endl
+		<< "Max: " << max << endl << "Min: " << min << endl;
 	return 0;
 }
