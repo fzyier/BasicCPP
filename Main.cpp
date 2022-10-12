930ddf085ae1eb84969f000f566422b4d85467d6
fzyier <babeckiy2004@gmail.com>
2022-10-12 14:44:03 +0300

    Exercise 2

--- Main.cpp
+++ Main.cpp
@@ -4,5 +4,27 @@ using namespace std;
 
 int main() {
 
+	char symb;
+	cout << "Enter symb: ";
+	cin >> symb;
+	int hor, ver;
+	cout << "Horizontal value: ";
+	cin >> hor;
+	cout << "Vertical value: ";
+	cin >> ver;
+
+	for (int i = 1; i <= hor; i++) {
+		cout << symb;
+	}
+	for (int i = 1; i <= ver; i++) {
+		cout <<endl<< symb;
+		for (int j = 1; j <= hor-2; j++)
+			cout << " ";
+		cout << symb;
+	}
+	cout << endl;
+	for (int i = 1; i <= hor; i++) {
+		cout << symb;
+	}
 	return 0;
 }
