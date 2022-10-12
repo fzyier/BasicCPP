40b0a9bf5374b42798aa687776c007c6249fb559
fzyier <babeckiy2004@gmail.com>
2022-10-12 14:12:25 +0300

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
+	char symb;
+	cout << "Enter a symb: ";
+	cin >> symb;
 
+	cout << symb << symb << symb << symb << symb << symb << symb << symb << endl;
+	cout << symb << "      " << symb << endl;
+	cout << symb << "      " << symb << endl;
+	cout << symb << symb << symb << symb << symb << symb << symb << symb << endl;
 	return 0;
 }
