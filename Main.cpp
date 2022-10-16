4d58efafc6e08ff2836c0278358315fef08d5b40
fzyier <babeckiy2004@gmail.com>
2022-10-16 18:54:46 +0300

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <iostream>
 
 using namespace std;
 
+int DegreeOfNumber(int num, int degree) {
+	return pow(num, degree);
+}
 int main() {
+	cout << DegreeOfNumber(1, 5);
 
 	return 0;
 }
