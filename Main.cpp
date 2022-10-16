43c370fbc11f486af4afa6d9f2880a80f5ec9017
fzyier <babeckiy2004@gmail.com>
2022-10-16 19:00:48 +0300

    Exercise 2

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,13 @@
 
 using namespace std;
 
+int Sum(int num1, int num2) {
+	int num = 0;
+	for (int i = num1 + 1; i < num2; i++)
+		num += i;
+	return num;
+}
 int main() {
-
+	cout << Sum(1, 5);
 	return 0;
 }
