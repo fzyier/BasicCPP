b4691d714af9f852fd92731fb2c089509ab3487f
fzyier <babeckiy2004@gmail.com>
2022-10-18 19:09:39 +0300

    Broke down

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,31 @@
 
 using namespace std;
 
-int main() {
+void HappyNumber(int number) {
+	int number_copy = number;
+	int move = 100000;
+	int sum1 = 0, sum2 = 0;
+	do {
+		number /= 10;
+		number_copy %= move;
+		move /= 10;
+	} while (number / 1000);
+	if (sum1 == sum2)
+		cout << "Happy Number";
+	else
+		cout << "Basic Number";
+}
 
+void NumberHappy(int num) {
+	num /= 10;
+	num %= 10;
+	cout << num;
+}
+
+int main() {
+	int num = 123456;
+	//cout << "Enter: ";
+	//cin >> num;
+	HappyNumber(num);
 	return 0;
 }
