b26edfc8b1d27bde00fab3dc9573024817795eff
fzyier <babeckiy2004@gmail.com>
2022-10-18 20:29:05 +0300

    Exercise 2

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,22 @@
 
 using namespace std;
 
-int main() {
+void Average(double arr[], int size) {
+	double average = 0;
+	for (int i = 0; i < size; i++)
+		average += arr[i];
+	cout << "Average is " << average / size;
+}
 
+int main() {
+	const int size = 7;
+	double arr[size];
+
+	for (int i = 0; i < size; i++) {
+		cout << "Enter a num of arrays: ";
+		cin >> arr[i];
+	}
+
+	Average(arr, size);
 	return 0;
 }
