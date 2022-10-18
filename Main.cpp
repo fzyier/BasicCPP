54360fd350ec52b2bb0ba8ed8f4e87e53cc1b112
fzyier <babeckiy2004@gmail.com>
2022-10-18 20:32:20 +0300

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,28 @@
 
 using namespace std;
 
-int main() {
+void PlusMinusZero(double arr[], int size) {
+	int minus = 0, plus = 0, zero = 0;
+	for (int i = 0; i < size; i++) {
+		if (arr[i] == 0)
+			zero++;
+		else if (arr[i] > 0)
+			plus++;
+		else
+			minus++;
+	}
+	cout << "Zeros: " << zero << endl << "Pluses: " << plus << "Minuses: " << minus;
+}
 
+int main() {
+	const int size = 7;
+	double arr[size];
+
+	for (int i = 0; i < size; i++) {
+		cout << "Enter a num of arrays: ";
+		cin >> arr[i];
+	}
+
+	PlusMinusZero(arr, size);
 	return 0;
 }
