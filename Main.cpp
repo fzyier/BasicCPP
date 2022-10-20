5882a87ef5c0d0fbc49dba9653491c1f68ad7f19
fzyier <babeckiy2004@gmail.com>
2022-10-20 19:33:27 +0300

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int main() {
+template <typename T>
+void Func(T p_d, T p_i) {
+	cout << setprecision(p_i) << p_d;
+}
 
+int main() {
+	double x;
+	int y;
+	cout << "Enter double:";
+	cin >> x;
+	cout << "Enter int:";
+	cin >> y;
+
+	Func<double>(x, y);
 	return 0;
 }
