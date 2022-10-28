dfd86343de2e0704cfada9c6ec3a9fe795f5252d
fzyier <babeckiy2004@gmail.com>
2022-10-29 02:26:11 +0300

    Exerices 3

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
 using namespace std;
-
+void PerfectNum(int num) {
+	int ultra = 0;
+	for (int i = 1; i <= 17; i++) {
+		if (ultra == num) {
+			cout << "Perfect Number!";
+			return;
+		}
+		if (num % i == 0)
+			ultra += i;
+	}
+	cout << "Not Perfect Number!";
+}
 int main() {
-
+	int num;
+	cin >> num;
+	PerfectNum(num);
 	return 0;
 }
