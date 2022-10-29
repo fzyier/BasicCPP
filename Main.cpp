a2f74ba779d8c0b196b6bf21185ff0708e09d2f3
fzyier <babeckiy2004@gmail.com>
2022-10-29 20:32:28 +0300

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
-int main() {
+void Rectangle(int N, int K) {
+	for (int i = 0; i < K; i++) {
+		for (int j = 0; j < N; j++)
+			cout << "*";
+		cout << endl;
+	}
+}
 
+int main() {
+	int N, K;
+	cout << "Enter length: ";
+	cin >> N;
+	cout << "Enter width: ";
+	cin >> K;
+
+	Rectangle(N, K);
 	return 0;
 }
