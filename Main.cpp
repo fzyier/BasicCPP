e1280ca4d9f06926b91054fa7041fb43c296f59f
fzyier <babeckiy2004@gmail.com>
2022-10-30 01:40:53 +0300

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
+int Func(int num1, int num2, int beginNum = 1, int endNum = 100) {
+	if (num1 % beginNum != 0 && num2 % beginNum != 0) {
+		if (beginNum == endNum)
+			return 0;
+		return Func(num1, num2, beginNum+=1);
+	}
+	else
+		return beginNum;
+}
+
 int main() {
 
+	cout << Func(8, 16, 3, 100);
 	return 0;
 }
