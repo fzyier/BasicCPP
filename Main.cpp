e296cc580755e1661a0ad1da48934e75eb084554
fzyier <babeckiy2004@gmail.com>
2022-10-29 22:26:11 +0300

    Exercise 5

--- Main.cpp
+++ Main.cpp
@@ -2,15 +2,21 @@
 
 using namespace std;
 
- int Cube(int num) {
-	 return num * num * num;
+ void Cube(int num1, int num2) {
+	 if (num1 > num2)
+		 cout << num1 << ">" << num2;
+	 else if (num1 < num2)
+		 cout << num1 << "<" << num2;
+	 else
+		 cout << num1 << "=" << num2;
 }
 
 int main() {
-	int num;
-	cout << "Enter a number" << endl;
-	cout << "> ";
-	cin >> num;
-	cout << Cube(num);
+	int num1, num2;
+	cout << "Enter a number: ";
+	cin >> num1;
+	cout << "Enter a number: ";
+	cin >> num2;
+	Cube(num1,num2);
 	return 0;
 }
