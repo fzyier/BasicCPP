23c3463d0f4fd53f633a4c3ec7c7d7fafb92e637
fzyier <babeckiy2004@gmail.com>
2022-10-29 21:35:31 +0300

    Exercise 2

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 
 using namespace std;
+// 1*2=2 2*3=6
+void Factorial(int num) {
+	int factorial = 2;
+	for (int i = 3; i <= num; i++)
+		factorial *= i;
+	cout << "Factorial " << factorial << endl;
+}
 
 int main() {
-
+	int num;
+	cout << "Enter a number: ";
+	cin >> num;
+	Factorial(num);
 	return 0;
 }
