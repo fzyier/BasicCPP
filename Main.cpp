802f0e5f5d1162a335327ad17911d80012fdb402
fzyier <babeckiy2004@gmail.com>
2022-10-29 20:02:27 +0300

    Exercise 5

--- Main.cpp
+++ Main.cpp
@@ -3,30 +3,25 @@
 using namespace std;
 
 void HappyNumber(int number) {
-	int number_copy = number;
-	int move = 100000;
+	int mult = 1, divi = 1000000;
 	int sum1 = 0, sum2 = 0;
-	do {
-		number /= 10;
-		number_copy %= move;
-		move /= 10;
-	} while (number / 1000);
-	if (sum1 == sum2)
-		cout << "Happy Number";
-	else
-		cout << "Basic Number";
-}
 
-void NumberHappy(int num) {
-	num /= 10;
-	num %= 10;
-	cout << num;
+	do {
+		mult *= 10;
+		divi /= 10;
+
+		sum1 += (number % mult) / (mult / 10);
+		sum2 += (number / divi) % (10 % (10*mult));
+	} while (divi != 1000);
+
+	(sum1 == sum2) ? cout << "Happy Number": cout << "Basic Number";
 }
 
 int main() {
-	int num = 123456;
-	//cout << "Enter: ";
-	//cin >> num;
+	int num;
+	cout << "Enter: ";
+	cin >> num;
+
 	HappyNumber(num);
 	return 0;
 }
