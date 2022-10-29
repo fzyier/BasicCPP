217e4e3d5d951671000faa3f1501850e83f0df59
fzyier <babeckiy2004@gmail.com>
2022-10-29 22:28:44 +0300

    Exercise 5

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
-int main() {
+void More(int num1, int num2) {
+	if (num1 > num2)
+		cout << num1 << ">" << num2;
+	else if (num1 < num2)
+		cout << num1 << "<" << num2;
+	else
+		cout << num1 << "=" << num2;
+}
 
+int main() {
+	int num1, num2;
+	cout << "Enter a number: ";
+	cin >> num1;
+	cout << "Enter a number: ";
+	cin >> num2;
+	More(num1, num2);
 	return 0;
 }
