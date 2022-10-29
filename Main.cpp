4832545a750e817dfb36b2af7c22da4ab52428e6
fzyier <babeckiy2004@gmail.com>
2022-10-30 01:12:35 +0300

    Exercise 9

--- Main.cpp
+++ Main.cpp
@@ -9,12 +9,27 @@ void Random(int arr[], const int size) {
 		arr[i] = 1 + rand() % 10;
 }
 
+int PrimeNums(int arr[], int size) {
+	int a = 0, b = 0;
+	for (int i = 0; i < size; i++) {
+		b = 0;
+		for (int j = 2; j < 100; j++) {
+			if (arr[i] == j)
+				continue;
+			if (arr[i] % j == 0)
+				b++;
+		}
+		if (b == 0)
+			a++;
+	}
+	return a;
+}
 
 int main() {
-	const int size = 6;
-	int arr[size];
-	Random(arr, size);
-
+	const int size = 5;
+	int arr[size] = {2,3,4,5,6};
+	//Random(arr, size);
+	cout << PrimeNums(arr,size) << " prime numbers!";
 
 
 	return 0;
