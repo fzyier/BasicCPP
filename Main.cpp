56801afe42f45398257ff7175c6d16aeedbcfe7b
fzyier <babeckiy2004@gmail.com>
2022-10-29 22:17:13 +0300

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,23 @@
 
 using namespace std;
 
-int main() {
+void PrimeNum(int num) {
+	for (int i = 2; i <= 100; i++) {
+		if (num == i)
+			continue;
+		if (num % i == 0) {
+			cout << num << " is NOT a prime number!!";
+			return;
+		}
+	}
+	cout << num << " is a prime number!!";
+}
 
+int main() {
+	int num;
+	cout << "Enter a number between 2-100" << endl;
+	cout << "> ";
+	cin >> num;
+	PrimeNum(num);
 	return 0;
 }
