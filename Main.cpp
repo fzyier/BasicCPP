358dcbc3a76fcf3dc021f947aa8314f33abc5258
fzyier <babeckiy2004@gmail.com>
2022-10-30 14:12:43 +0200

    Exercise 2

--- Main.cpp
+++ Main.cpp
@@ -1,19 +1,83 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int Func(int num1, int num2, int beginNum = 1, int endNum = 100) {
-	if (num1 % beginNum != 0 && num2 % beginNum != 0) {
-		if (beginNum == endNum)
-			return 0;
-		return Func(num1, num2, beginNum+=1);
+void Random(char arr[], int size) {
+	srand((unsigned)time(0));
+
+	for (size_t i = 0; i < size; i++)
+		arr[i] = 48+(rand() % 10);
+}
+
+int Bulls(char arr[],char trueArray[], int size) {
+	int arr0[10]{0,0,0,0,0,0,0,0,0,0};
+	int arr1[10]{0,0,0,0,0,0,0,0,0,0};
+	for (int i = 0; i < size; i++) {
+		int num = 0;
+		for (int j = 0; j < 10; j++) {
+			if ((char)(j + 48) == arr[i]) {
+				num++;
+				arr0[j] += num;
+			}
+
+		}
 	}
-	else
-		return beginNum;
+
+	for (int i = 0; i < size; i++) {
+		int num = 0;
+		for (int j = 0; j < 10; j++) {
+			if ((char)(j + 48) == trueArray[i]) {
+				num++;
+				arr1[j] += num;
+			}
+
+		}
+	}
+
+	int num = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		if (arr1[i] <= arr0[i] && arr1[i] != 0)
+			num+=arr1[i];
+	}
+	return num;
+}
+int Cows(char arr[], char trueArray[], int size) {
+	int num = 0;
+	for (int i = 0; i < size; i++) {
+		if (trueArray[i] == arr[i])
+			num++;
+	}
+	return num;
+}
+void BullsAndCows(char trueArray[], int attempt = 0) {
+	const int size = 5;
+	char arr[size] = { NULL, NULL, NULL, NULL, NULL };
+
+	cout << "Enter a number!" << endl;
+	
+	cout << "Enter: ";
+	cin.getline(arr, 32);
+	cout << Bulls(arr, trueArray, size) << "Bulls" << endl;
+	cout << Cows(arr,trueArray, size) << "Cows" << endl;
+
+	if (arr[0] != trueArray[0] || arr[1] != trueArray[1]
+		|| arr[2] != trueArray[2] || arr[3] != trueArray[3]) {
+		attempt++;
+		return BullsAndCows(trueArray, attempt);
+	}
+	else {
+		attempt++;
+		cout << "You guessed for " << attempt << " attempts!";
+	}
 }
 
 int main() {
+	const int size = 4;
+	char arr[size];
+	Random(arr, size);
 
-	cout << Func(8, 16, 3, 100);
+	BullsAndCows(arr);
 	return 0;
 }
