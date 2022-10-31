b5680e206edcedf64e799d359623c824020a16b2
fzyier <babeckiy2004@gmail.com>
2022-10-31 16:14:48 +0200

    Exercise 2

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,19 @@
 
 using namespace std;
 
-int main() {
+void Star(int N, int value = 0) {
+	if (N != value) {
+		cout << "*";
+		return Star(N, value + 1);
+	}
+	else
+		return;
+}
 
+int main() {
+	int N = 0;
+	cout << "Enter a value: ";
+	cin >> N;
+	Star(N);
 	return 0;
 }
