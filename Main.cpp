ec85c49cd339d342ada0d8dc23a32a62e130e4bf
fzyier <babeckiy2004@gmail.com>
2022-10-31 16:41:48 +0200

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
-int main() {
+void Between(int begin, int end, int amount = 0) {
+	if (begin != end+1) {
+		amount += begin; 
+		return Between(begin+1, end, amount);
+	}
+	else
+		cout << "TA DA!!" << endl << amount;
+}
 
+int main() {
+	int begin = 0, end = 0;
+	cout << "Enter a begin: ";
+	cin >> begin;
+	cout << "Enter an end: ";
+	cin >> end;
+	Between(begin, end);
 	return 0;
 }
