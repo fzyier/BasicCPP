d6123713d9edeeaf666e765d220026df24ca7d3c
fzyier <babeckiy2004@gmail.com>
2022-10-31 12:26:30 +0200

    Exercise 1

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,24 @@
 
 using namespace std;
 
-int main() {
+void FindPow(int userNum, int num = 1, int power = 1, int max = 100) {
+	int chNum = 0;
+	for(int i = power; i <= max; i++)
+		if (pow(num, i) == userNum) {
+			chNum = (int)pow(num, power);
+			cout << userNum << " = " << num << "^" << i << endl;
+			break;
+		}
+	if (chNum != userNum)
+		return FindPow(userNum, num++);
+}
 
+int main() {
+	int num, power;
+	cout << "Enter a number: ";
+	cin >> num;
+	cout << "Enter a power of the number: ";
+	cin >> power;
+	FindPow(num);
 	return 0;
 }
