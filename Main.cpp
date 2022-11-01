394ed5a06374ad5542a76512d27f383bd53dbf76
fzyier <babeckiy2004@gmail.com>
2022-11-01 19:05:05 +0200

    Exercise 3

--- Main.cpp
+++ Main.cpp
@@ -2,15 +2,23 @@
 
 using namespace std;
 
-char PlusMinus(int num) {
-	return(num >= 0) ? '+' : '-';
+void Replace(int *num1, int *num2) {
+	int num3 = *num1;
+	*num1 = *num2;
+	*num2 = num3;
 }
 
 int main() {
-	int* num = new int{};
+	int* num1 = new int{};
 	cout << "Enter: ";
-	cin >> *num;
-	cout << PlusMinus(*num);
-	delete num;
+	cin >> *num1;
+	int* num2 = new int{};
+	cout << "Enter: ";
+	cin >> *num2;
+
+	Replace(num1, num2);
+
+	cout << "num1 = " << *num1 << "\nnum2 = " << *num2 << endl;
+	delete num1, num2;
 	return 0;
 }
