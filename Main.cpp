2bcf2bf62c1459aeb6a508840274188bb6f7a18b
fzyier <babeckiy2004@gmail.com>
2022-11-01 19:13:51 +0200

    Exercise 4

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,20 @@
 
 using namespace std;
 
-void Replace(int *num1, int *num2) {
-	int num3 = *num1;
-	*num1 = *num2;
-	*num2 = num3;
+int Plus(int* num1, int* num2) {
+	return *num1 + *num2;
+}
+
+int Minus(int* num1, int* num2) {
+	return *num1 - *num2;
+}
+
+int Multiply(int* num1, int* num2) {
+	return *num1 * *num2;
+}
+
+int Division(int* num1, int* num2) {
+	return *num1 / *num2;
 }
 
 int main() {
@@ -16,9 +26,36 @@ int main() {
 	cout << "Enter: ";
 	cin >> *num2;
 
-	Replace(num1, num2);
+	int choose;
+	do {
+		cout << "What do you want?" << endl;
+		cout << "1) " << *num1 << " + " << *num2 << endl;
+		cout << "2) " << *num1 << " - " << *num2 << endl;
+		cout << "3) " << *num1 << " * " << *num2 << endl;
+		cout << "4) " << *num1 << " / " << *num2 << endl;
+		cout << "0) exit" << endl;
+		cin >> choose;
 
-	cout << "num1 = " << *num1 << "\nnum2 = " << *num2 << endl;
+		switch (choose) {
+		case 1: {
+			cout << Plus(num1, num2) << endl;
+			break;
+		}
+		case 2: {
+			cout << Minus(num1, num2) << endl;
+			break;
+		}
+		case 3: {
+			cout << Multiply(num1, num2) << endl;
+			break;
+		}
+		case 4: {
+			cout << Division(num1, num2) << endl;
+			break;
+		}
+		}
+	} while (choose != 0);
+
 	delete num1, num2;
 	return 0;
 }
