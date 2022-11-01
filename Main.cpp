55b127ff3292af8ace0bbe6799271eb775297652
fzyier <babeckiy2004@gmail.com>
2022-11-01 19:59:44 +0200

    Exercise 2

--- Main.cpp
+++ Main.cpp
@@ -2,11 +2,30 @@
 
 using namespace std;
 
+void Random(int arr[]) {
+	for (int i = 0; i <= sizeof(*arr); i++)
+		arr[i] = 1 + rand() % 15;
+}
+void Output(int arr[]) {
+	for (int i = 0; i <= sizeof(*arr); i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+void ReverseOutput(int arr[]) {
+	for (int i = sizeof(*arr); i >= 0; i--)
+		cout << arr[i] << " ";
+	cout << endl;
+}
 int main() {
 	int size;
 	cout << "Enter a size of an array: ";
 	cin >> size;
 
 	int* arr = new int[size];
+	Random(arr);
+	Output(arr);
+	ReverseOutput(arr);
+
+
 	return 0;
 }
