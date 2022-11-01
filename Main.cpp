bbc8250080927b580def9b44139d0782e2f1bb14
fzyier <babeckiy2004@gmail.com>
2022-11-01 19:40:00 +0200

    Exercise 5

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,28 @@
 
 using namespace std;
 
-int main() {
+int SumOfArr(int arr[]) {
+	int sum = 0;
+	for (int i = 0; i <= sizeof(*arr); i++)
+ 			sum += arr[i];
 
+	return sum;
+}
+
+int main() {
+	srand((unsigned)time(0));
+	int size;
+	cout << "Enter a size of an array: ";
+	cin >> size;
+
+	int* arr = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		arr[i] = 1 + rand() % 15;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	cout << SumOfArr(arr);
 	return 0;
 }
