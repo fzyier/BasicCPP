66df608c61e789c2d2e6c3eb01b8617d651cb34d
fzyier <babeckiy2004@gmail.com>
2022-11-20 02:59:12 +0200

    Exercise 1

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,5 +1,15 @@
 #include "../Core/HeaderLib/Header.h"
 
 int main() {
+	char arr[32];
+	std::cout << "Enter: ";
+	fgets(arr, sizeof(arr), stdin);
+
+	for (char* i = arr; *i; i++)
+		if (*i == ' ')
+			*i = '\t';
+
+	std::cout << arr;
+
 	return 0;
 }
