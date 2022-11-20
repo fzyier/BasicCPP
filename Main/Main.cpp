239fa75a13134e186afc9424cf2c87cd8c9c8504
fzyier <babeckiy2004@gmail.com>
2022-11-20 04:29:04 +0200

    Exercise 3

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,5 +1,15 @@
 #include "../Core/HeaderLib/Header.h"
 
 int main() {
+	const int size = 32;
+	char arr[size];
+	std::cout << "Enter: ";
+	fgets(arr, size, stdin);
+	
+	int spaces = 0;
+	for(char* i = arr; *i; i++)
+		if (isspace(*i))
+			spaces++;
+	std::cout << spaces << " words!" << std::endl;
 	return 0;
 }
