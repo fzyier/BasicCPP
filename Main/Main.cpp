7be5e3076968bf8a65c3c6d0f409c41265ef309e
fzyier <babeckiy2004@gmail.com>
2022-11-20 03:35:24 +0200

    Exercise 2

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,6 +1,22 @@
 #include "../Core/HeaderLib/Header.h"
 
 int main() {
-	
+	const int size = 32;
+	char arr[size];
+	fgets(arr, size, stdin);
+
+	int digits = 0, alphas = 0, other = 0;
+	for (char* i = arr; *i; i++) {
+		if (isdigit(*i))
+			digits++;
+		else if (isalpha(*i))
+			alphas++;
+		else
+			other++;
+	}
+
+	std::cout << "Digits = " << digits << std::endl;
+	std::cout << "Alphas = " << alphas << std::endl;
+	std::cout << "Other = " << other << std::endl; // '/0' is a symbol.
 	return 0;
 }
