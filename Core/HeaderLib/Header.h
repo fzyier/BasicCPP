52f40af3d44c4bc46b4724f0f202abd2bb4bdd09
fzyier <babeckiy2004@gmail.com>
2022-12-01 19:37:15 +0200

    Exercise 2

--- Core/HeaderLib/Header.h
+++ Core/HeaderLib/Header.h
@@ -1,18 +1,5 @@
 #include <iostream>
 
 int NumberToString(char *str) {
-	//switch (*str){
-	//case '0': return 0;
-	//case '1': return 1;
-	//case '2': return 2;
-	//case '3': return 3;
-	//case '4': return 4;
-	//case '5': return 5;
-	//case '6': return 6;
-	//case '7': return 7;
-	//case '8': return 8;
-	//case '9': return 9;
-	//default:  return 0;
-	//}
 	return (int)((*str) - 48);
 }
--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,7 +1,7 @@
 #include "../Core/HeaderLib/Header.h"
 
 int main() {
-	char ch = '1';
+	char ch = '7';
 	std::cout << NumberToString(&ch);
 	return 0;
 }
