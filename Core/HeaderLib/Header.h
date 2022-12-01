6038e866f331521108866ae846fa7b49f197de86
fzyier <babeckiy2004@gmail.com>
2022-12-01 19:35:09 +0200

    Exercise 2

--- Core/HeaderLib/Header.h
+++ Core/HeaderLib/Header.h
@@ -1,15 +1,18 @@
 #include <iostream>
 
-int mystrcmp(const char* str1, const char* str2) {
-	int size1(0), size2(0);
-	for (size_t i = 0; str1[i] != '\0'; i++)
-		size1++;
-	for (size_t i = 0; str2[i] != '\0'; i++)
-		size2++;
-	if (size1 == size2)
-		return 0;
-	else if (size1 > size2)
-		return 1;
-	else
-		return -1;
+int NumberToString(char *str) {
+	//switch (*str){
+	//case '0': return 0;
+	//case '1': return 1;
+	//case '2': return 2;
+	//case '3': return 3;
+	//case '4': return 4;
+	//case '5': return 5;
+	//case '6': return 6;
+	//case '7': return 7;
+	//case '8': return 8;
+	//case '9': return 9;
+	//default:  return 0;
+	//}
+	return (int)((*str) - 48);
 }
--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,9 +1,7 @@
 #include "../Core/HeaderLib/Header.h"
 
 int main() {
-	const char* str1 = { "HELLO!" };
-	const char* str2 = { "HELLO!" };
-
-	std::cout << mystrcmp(str1, str2);
+	char ch = '1';
+	std::cout << NumberToString(&ch);
 	return 0;
 }
