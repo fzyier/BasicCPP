6c58740e27262f32398cd4b33ee9b9ff29749e5b
fzyier <babeckiy2004@gmail.com>
2022-12-01 18:59:37 +0200

    Exercise 1

--- Core/HeaderLib/Header.h
+++ Core/HeaderLib/Header.h
@@ -1,1 +1,15 @@
 #include <iostream>
+
+int mystrcmp(const char* str1, const char* str2) {
+	int size1(0), size2(0);
+	for (size_t i = 0; str1[i] != '\0'; i++)
+		size1++;
+	for (size_t i = 0; str2[i] != '\0'; i++)
+		size2++;
+	if (size1 == size2)
+		return 0;
+	else if (size1 > size2)
+		return 1;
+	else
+		return -1;
+}
--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,5 +1,9 @@
 #include "../Core/HeaderLib/Header.h"
 
 int main() {
+	const char* str1 = { "HELLO!" };
+	const char* str2 = { "HELLO!" };
+
+	std::cout << mystrcmp(str1, str2);
 	return 0;
 }
