faaa2856a5e780553e11d95937a91c3db715a3c2
fzyier <babeckiy2004@gmail.com>
2022-12-02 07:14:58 +0200

    Exercise 1

--- Core/HeaderLib/Header.h
+++ Core/HeaderLib/Header.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 
 
 int mystrcmp(const char* str1, const char* str2) {
@@ -15,25 +16,35 @@ int mystrcmp(const char* str1, const char* str2) {
 		return -1;
 }
 
-int NumberToString(char* str) {
+int StringToNumber(char* str) {
 	return (int)((*str) - 48);
 }
 
-int mystrcmp(const char* str1, const char* str2) {
-	int size1(0), size2(0);
-	for (size_t i = 0; str1[i] != '\0'; i++)
-		size1++;
-	for (size_t i = 0; str2[i] != '\0'; i++)
-		size2++;
-	if (size1 == size2)
-		return 0;
-	else if (size1 > size2)
-		return 1;
-	else
-		return -1;
-}
-
-char* StringToNumber(int number) {
+char* NumberToString(int number) {
 	char ch = number+48;
 	return &ch;
 }
+
+char* Uppercase(char* str1) {
+	for (int i = 0; str1[i] != '\0'; i++)
+		str1[i] = str1[i] - 32;
+	return str1;
+}
+
+char* Lowercase(char* str1) {
+	for (int i = 0; str1[i] != '\0'; i++)
+		str1[i] = str1[i] + 32;
+	return str1;
+}
+
+char* mystrrev(char* str) {
+	int size = 0;
+	for (size_t i = 0; str[i] != '\0'; i++)
+		size++;
+
+	for (int i = 0; i < size/2; i++)
+		std::swap(str[i],str[size-i]);
+
+	return str;
+}
+
--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,7 +1,51 @@
 #include "../Core/HeaderLib/Header.h"
 
 int main() {
-	int ch = 7;
-	std::cout << *StringToNumber(ch);
+	//Mystrcmp
+	const char* str1 = { "Hello!" };
+	const char* str2 = { "Hello!" };
+	std::cout << mystrcmp(str1,str2) << std::endl;
+	
+	//NumberToString
+	std::cout << *NumberToString(7) << std::endl;
+	
+	//StringToNumber
+	char  ch = '1';
+	std::cout << StringToNumber(&ch);
+	 
+	//Upper Case
+	std::cout << std::endl << ">  UPPER CASE\n";
+	char str3[] = {"hello"};
+	
+	for (int i = 0; str3[i] != '\0'; i++)
+		std::cout << str3[i] << " ";
+	
+	std::cout << std::endl;
+	
+	*str3 = *(Uppercase(&str3[0]));
+	for (int i = 0; str3[i] != '\0'; i++)
+		std::cout << str3[i] << " ";
+	
+	//Lower Case
+	std::cout << std::endl << ">  LOWERCASE\n";
+	char str4[] = { "HELLO" };
+	
+	for (int i = 0; str4[i] != '\0'; i++)
+		std::cout << str4[i] << " ";
+	std::cout << std::endl;
+	
+	*str4 = *(Lowercase(&str4[0]));
+	
+	for (int i = 0; str4[i] != '\0'; i++)
+		std::cout << str4[i] << " ";
+	std::cout << std::endl;
+
+	//Reverse
+	char* str5 = new char[32] {"Hello"};
+	str5 = mystrrev(str5);
+
+	for (int i = 0; i < 32; i++)
+		std::cout << str5[i] << " ";
+
 	return 0;
 }
