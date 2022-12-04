d2c094669fa44236ec97ed19ef6ffdc56515da18
npax1 <prakh55555@gmail.com>
2022-12-04 19:15:54 +0200

    Task1

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,5 +1,41 @@
-#include "../Core/HeaderLib/Header.h"
+#include "Header.h"
+
+using namespace std;
+
+void Initializing_surnames(string* surnames, int size) {
+	for (size_t i = 0; i < size; i++) {
+		cout << "Enter: ";
+		cin >> surnames[i];
+	}
+}
+void Initializing_print(string* surnames, int size) {
+	for (size_t i = 0; i < size; i++) {
+		cout << surnames[i] << " ";
+	}
+	cout << endl;
+}
+void SortArray(string* surnames, int size) {
+	for (int i = 1; i < size; i++)
+	{
+		string temp = surnames[i];
+
+		int j = i - 1;
+		while (j >= 0 && temp.length() < surnames[j].length())
+		{
+			surnames[j + 1] = surnames[j];
+			j--;
+		}
+		surnames[j + 1] = temp;
+	}
+}
 
 int main() {
-	return 0;
+	int size = 5;
+	string* surnames = new string[size];
+
+	Initializing_surnames(surnames, size);
+	Initializing_print(surnames, size);
+	SortArray(surnames, size);
+	Initializing_print(surnames, size);
+
 }
