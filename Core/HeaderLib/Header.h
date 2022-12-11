19167f038f60214fb46bf490ef3a3ca18bba7161
fzyier <babeckiy2004@gmail.com>
2022-12-11 06:40:01 +0200

    Exercise 1

--- Core/HeaderLib/Header.h
+++ Core/HeaderLib/Header.h
@@ -1,1 +1,23 @@
 #include <iostream>
+
+struct ComplexNums {
+public:
+	int first_numer;
+	int second_number;
+	void Sum(int num1, int num2) {
+		std::cout << "SUM: " << std::endl;
+		std::cout << num1 + num2 << std::endl;
+	}
+	void Sub(int num1, int num2) {
+		std::cout << "SUB: " << std::endl;
+		std::cout << num1 - num2 << std::endl;
+	}
+	void Mul(int num1, int num2) {
+		std::cout << "MUL: " << std::endl;
+		std::cout << num1 * num2 << std::endl;
+	}
+	void Div(int num1, int num2) {
+		std::cout << "DIV: " << std::endl;
+		std::cout << num1 / num2 << std::endl;
+	}
+};
--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,5 +1,11 @@
 #include "../Core/HeaderLib/Header.h"
 
 int main() {
+	ComplexNums nums{3,3};
+	nums.Sum(nums.first_numer,nums.second_number);
+	nums.Sub(nums.first_numer, nums.second_number);
+	nums.Mul(nums.first_numer, nums.second_number);
+	nums.Div(nums.first_numer, nums.second_number);
+
 	return 0;
 }
