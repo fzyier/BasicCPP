491ddd37694213d7df008a7e2ac514e4cb979134
fzyier <babeckiy2004@gmail.com>
2022-12-12 20:02:01 +0200

    Exercise 1

--- Core/HeaderLib/Header.h
+++ Core/HeaderLib/Header.h
@@ -1,1 +1,39 @@
 #include <iostream>
+#include <string>
+
+struct WashingMachine
+{
+private:
+	std::string firm;
+	std::string color;
+	double width;
+	double length;
+	double height;
+	double power;
+	double speed;
+	double temperature;
+public:
+	void Print()
+	{
+		system("cls");
+		std::cout << "Firm: " << firm << std::endl;
+		std::cout << "Color: " << color << std::endl;
+		std::cout << "Width: " << width << std::endl;
+		std::cout << "Length: " << length << std::endl;
+		std::cout << "Height: " << height << std::endl;
+		std::cout << "Power: " << power << std::endl;
+		std::cout << "Speed: " << speed << std::endl;
+		std::cout << "Temperature: " << temperature << std::endl;
+	}
+	void Input() 
+	{
+		std::cout << "Firm: "; std::getline(std::cin, firm);
+		std::cout << "Color: "; std::getline(std::cin, color);
+		std::cout << "Width: "; std::cin >> width;
+		std::cout << "Length: "; std::cin >> length;
+		std::cout << "Height: "; std::cin >> height;
+		std::cout << "Power: "; std::cin >> power;
+		std::cout << "Speed: "; std::cin >> speed;
+		std::cout << "Temperature: "; std::cin >> temperature;
+	}
+};
--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,5 +1,8 @@
 #include "../Core/HeaderLib/Header.h"
 
 int main() {
+	WashingMachine wm{};
+	wm.Input();
+	wm.Print();
 	return 0;
 }
