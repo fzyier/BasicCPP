6475f44e55cc439bd8e08b3ed0e987113edf6427
fzyier <babeckiy2004@gmail.com>
2022-12-12 17:53:17 +0200

    Exercise 1

--- Core/HeaderLib/Header.h
+++ Core/HeaderLib/Header.h
@@ -1,1 +1,332 @@
+#include <string>
 #include <iostream>
+#include <Windows.h>
+
+void phone_model() {
+	std::cout << '\t' << char(201) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(187) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(219) << char(178) << char(178) << char(178) << char(178) << char(219) << char(178) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(186) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(186) << std::endl;
+	std::cout << '\t' << char(186) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(200) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(188) << std::endl;
+	system("pause");
+	system("cls");
+	Sleep(500);
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(178) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	system("pause");
+	system("cls");
+	Sleep(500);
+	std::cout << '\t' << char(201) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(187) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(233) << char(233) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(186) << char(219) << char(233) << char('0') << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(186) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(233) << char(248) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(186) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(219) << char(219) << char(219) << char('J') << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(179) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(219) << char(179) << std::endl;
+	std::cout << '\t' << char(200) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(196) << char(188) << std::endl;
+	system("pause");
+	system("cls");
+	Sleep(500);
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(178) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(178) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	std::cout << '\t' << ' ' << ' ' << ' ' << ' ' << ' ' << char(219) << std::endl;
+	system("pause");
+	system("cls");
+	Sleep(500);
+}
+struct Connection 
+{
+private:
+	short sim_value;
+	std::string sim_size;
+	bool sim_support;
+public:
+	void Print() 
+	{
+		std::cout << "Numbers of sim-card: " << sim_value << std::endl;
+		std::cout << "Size of sim-card: " << sim_size << std::endl;
+		std::cout << "Sim-card: " << sim_support << std::endl;
+	}
+	void Input() 
+	{
+		std::cout << "Number of sim-card: ";
+		std::cin >> sim_value;
+		std::cout << "Size of sim-card: ";
+		std::cin.ignore();
+		std::getline(std::cin,sim_size);
+		std::cout << "Sim-card support: ";
+		std::cin >> sim_support;
+	}
+};
+
+struct OC
+{
+private:
+	std::string operation_system;
+public:
+	void Print() 
+	{
+		std::cout << "OC: " << operation_system << std::endl;
+	}
+	void Input() 
+	{
+		std::cout << "OC: ";
+		std::cin.ignore();
+		std::getline(std::cin, operation_system);
+	}
+};
+
+struct Cheap
+{
+private:
+	std::string model;
+	short cores;
+public:
+	void Print() 
+	{
+		std::cout << "Model: " << model << std::endl;
+		std::cout << "Cores: " << cores << std::endl;
+	}
+	void Input() 
+	
+	{
+		std::cout << "Model: ";
+		std::cin.ignore();
+		std::getline(std::cin, model);
+		std::cout << "Numbers of cores: ";
+		std::cin >> cores;
+	}
+};
+
+struct Storage
+{
+private:
+	short storage_value;
+public:
+	void Print()
+	{
+		std::cout << "Storage: " << storage_value << std::endl;
+	}
+	void Input()
+	{
+		std::cout << "Storage value: ";
+		std::cin >> storage_value;
+	}
+};
+
+struct Camera
+{
+private:
+	std::string camera_main;
+	short camera_frontal;
+	bool camera_flashing;
+	std::string camera_stabilization;
+	std::string camera_zoom;
+public:
+	void Print() 
+	{
+		std::cout << "Main camera: " << camera_main << std::endl;
+		std::cout << "Frontal camera: " << camera_frontal << std::endl;
+		std::cout << "Flashing: " << camera_flashing << std::endl;
+		std::cout << "Stabilisation: " << camera_stabilization << std::endl;
+		std::cout << "Zoom: " << camera_zoom << std::endl;
+	}
+	void Input() 
+	{
+		std::cin.ignore();
+		std::cout << "Main camera: "; std::getline(std::cin,camera_main);
+		std::cout << "Frontal camera: "; std::cin >> camera_frontal;
+		std::cout << "Flashing: "; std::cin >> camera_flashing;
+		std::cin.ignore();
+		std::cout << "Stabilisation: "; std::getline(std::cin,camera_stabilization);
+		std::cout << "Zoom: "; std::getline(std::cin,camera_zoom);
+	}
+};
+
+struct Interfaces
+{
+private:
+	std::string wifi;
+	short bluetooth;
+	std::string navigation_systems;
+	bool NFC;
+	std::string usb_interface;
+public:
+	void Print() 
+	{
+		std::cout << "Wifi: " << wifi << std::endl;
+		std::cout << "Bluetooth: " << bluetooth << std::endl;
+		std::cout << "Navigation systems: " << navigation_systems << std::endl;
+		std::cout << "NFC:" << NFC << std::endl;
+		std::cout << "Usb interface: " << usb_interface << std::endl;
+	}
+	void Input() 
+	{
+		std::cout << "Wifi: "; std::getline(std::cin, wifi);
+		std::cout << "Bluetooth: "; std::cin >> bluetooth;
+		std::cin.ignore();
+		std::cout << "Navigation systems: "; std::getline(std::cin, navigation_systems);
+		std::cout << "NFC: "; std::cin >> NFC;
+		std::cin.ignore();
+		std::cout << "Usb interface: "; std::getline(std::cin, usb_interface);
+	}
+};
+
+struct Battery
+{
+private:
+	bool charging_fast;
+	bool charging_wireless;
+	bool charging_power;
+public:
+	void Print()
+	{
+		std::cout << "Fast charging: " << charging_fast << std::endl;
+		std::cout << "Wireless charging: " << charging_wireless << std::endl;
+		std::cout << "Power charging: " << charging_power << std::endl;
+	}
+	void Input() 
+	{
+		std::cout << "Fast charging: "; std::cin >> charging_fast;
+		std::cout << "Wireless charging: "; std::cin >> charging_wireless;
+		std::cout << "Power charging: "; std::cin >> charging_power;
+	}
+};
+
+struct Body
+{
+private:
+	std::string protection_body;
+	std::string protection_class;
+	std::string protection_biometric;
+	
+	std::string kit;
+	
+	std::string body_material;
+	double body_height;
+	double body_width;
+	double body_deep;
+
+	double height;
+
+	std::string color;
+
+	short waranty;
+
+	std::string manufacturer;
+
+	std::string brand;
+public:
+	void Print()
+	{
+		std::cout << "Body protection: " << protection_body << std::endl;
+		std::cout << "Class protection: " << protection_class << std::endl;
+		std::cout << "Biometric protection: " << protection_biometric << std::endl;
+
+		std::cout << "Starter pack: " << kit << std::endl;
+		
+		std::cout << "Body material: " << body_material << std::endl;
+		std::cout << "Body height: " << body_height << std::endl;
+		std::cout << "Body width: " << body_width << std::endl;
+		std::cout << "Body deep: " << body_deep << std::endl;
+
+		std::cout << "Total height: " << height << std::endl;
+		
+		std::cout << "Color: " << color << std::endl;
+
+		std::cout << "Waranty period: " << waranty << std::endl;
+
+		std::cout << "Country of manufacturing: " << manufacturer << std::endl;
+
+		std::cout << "Brand: " << brand << std::endl;
+	}
+	void Input() 
+	{
+		std::cin.ignore();
+		std::cout << "Body protection: "; std::getline(std::cin,protection_body);
+		//std::cin.ignore();
+		std::cout << "Class protection: "; std::getline(std::cin, protection_class);
+		//std::cin.ignore();
+		std::cout << "Biometric protection: "; std::getline(std::cin, protection_biometric);
+		//std::cin.ignore();
+		std::cout << "Starter pack: "; std::getline(std::cin, kit);
+		//std::cin.ignore();
+		std::cout << "Body material: "; std::getline(std::cin, body_material);
+		std::cout << "Body height: "; std::cin >> body_height;
+		std::cout << "Body width: "; std::cin >> body_width;
+		std::cout << "Body deep: "; std::cin >> body_deep;
+
+		std::cout << "Total height: "; std::cin >> height;
+
+		std::cin.ignore();
+		std::cout << "Color: "; std::getline(std::cin, color);
+		std::cout << "Waranty period: "; std::cin >> waranty;
+		std::cin.ignore();
+		std::cout << "Country of manufacturing: "; std::getline(std::cin, manufacturer);
+		//std::cin.ignore();
+		std::cout << "Brand: "; std::getline(std::cin, brand);
+	}
+};
+
+struct Phone
+{
+private:
+	Connection connection{};
+	OC oc{};
+	Cheap cheap{};
+	Storage storage{};
+	Camera camera{};
+	Interfaces interfaces{};
+	Battery battery{};
+	Body body{};
+public:
+	void Print() 
+	{
+		system("cls");
+		connection.Print();
+		oc.Print();
+		cheap.Print();
+		storage.Print();
+		camera.Print();
+		interfaces.Print();
+		battery.Print();
+		body.Print();
+		system("pause");
+		system("cls");
+		Sleep(500);
+		phone_model();
+	}
+	void Input() 
+	{
+		connection.Input();
+		oc.Input();
+		cheap.Input();
+		storage.Input();
+		camera.Input();
+		interfaces.Input();
+		battery.Input();
+		body.Input();
+	}
+};
