9775e71309a644111e2effcb0bda6ac499ccfc75
fzyier <babeckiy2004@gmail.com>
2022-12-13 07:54:45 +0200

    Exercise 1

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,8 +1,8 @@
 #include "../Core/HeaderLib/Header.h"
 
 int main() {
-	Iron* iron = new Iron;
-	iron->Input();
-	iron->Print();
-	delete iron;
+	WashingMachine wm{};
+	wm.Input();
+	wm.Print();
+	return 0;
 }
