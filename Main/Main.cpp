e7941ebe9e8afa27e59a94881e5594b35b4c8d26
fzyier <babeckiy2004@gmail.com>
2022-12-20 17:44:22 +0200

    Exercise 3

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -6,23 +6,36 @@
 
 
 int main() {
-	char file_path[] = "file.txt";
-	char file_text[256];
-	FILE* file = fopen(file_path, "w");
+	char file1_path[] = "file1.txt";
+	char file1_text[256];
+	FILE* file1 = fopen(file1_path, "r");
 
-	printf("Enter line: ");
-	scanf("%[^\n]s",file_text);
+	char file2_path[] = "file2.txt";
+	FILE* file2 = fopen(file2_path, "w");
 
-	for (size_t i = 0;file_text[i]!='\0'; i++) {
-		if ((int)file_text[i] + 3 > 122)
-			file_text[i] = 97 + ((file_text[i] + 3) - 122);
-		else
-			file_text[i] += 3;
+	while (fgets(file1_text, 256, file1)) {
+		for (int i = 0; file1_text[i] != '\n' && file1_text[i] != '\0'; i++) {
+			if ((int)file1_text[i] + 3 > 122)
+				file1_text[i] = 97 + ((file1_text[i] + 2) - 122);
+			else
+				file1_text[i] += 3;
+		}
+		fputs(file1_text, file2);
 	}
+	//printf("Enter line: ");
+	//scanf("%[^\n]s",file_text);
 
-	fputs(file_text, file);
+	//for (size_t i = 0;file_text[i]!='\0'; i++) {
+	//	if ((int)file_text[i] + 3 > 122)
+	//		file_text[i] = 97 + ((file_text[i] + 2) - 122);
+	//	else
+	//		file_text[i] += 3;
+	//}
+
+	//fputs(file_text, file);
 	
 
-	fclose(file);
+	fclose(file1);
+	fclose(file2);
 	return 0;
 } 
